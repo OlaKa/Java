605c3c278c63ba2513d9af26176cc1620b3361f8
GIT Admin <ola.karlsson@bahnhof.se>
2016-08-30 21:57:38 +0200

    Added first file

---
+++ javafiler/lektion1/Hej5Sound.java
@@ -0,0 +1,24 @@
+import javax.swing.*;
+import java.awt.*;
+import java.util.concurrent.TimeUnit;
+
+public class Hej5Sound {
+
+            public static void main (String[] arg) {
+
+                          JOptionPane.showMessageDialog(null, "Hej!\n" + "Number of processors: " + Runtime.getRuntime().availableProcessors() + "\n" + "Free memory:" + Runtime.getRuntime().freeMemory());
+                         for(int i=0; i<5;i++){
+			     Toolkit.getDefaultToolkit().beep();
+			     try {
+				 Thread.sleep(1000);                 //1000 milliseconds is one second.
+			     } catch(InterruptedException ex) {
+				 Thread.currentThread().interrupt();
+			     }
+
+			 }
+
+            }
+
+ 
+
+}
