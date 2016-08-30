400869b504693f82fad80adc54790de6105cf769
GIT Admin <ola.karlsson@bahnhof.se>
2016-08-30 23:07:41 +0200

    Added exercise 1.8/1.1

---
+++ javafiler/lektion1/Date.java
@@ -0,0 +1,14 @@
+import javax.swing.JOptionPane;
+import java.util.*;
+
+
+//Author Ola Karlsson
+
+public class Date {
+
+	
+	public static void main(String[] args) {
+		JOptionPane.showMessageDialog(null, "Datum och Tid:\n" + Calendar.getInstance().getTime().toString());
+	}
+
+}
