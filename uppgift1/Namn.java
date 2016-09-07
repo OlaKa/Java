baf8ca5720f5c0f652b035b3034b7aaff44af3f9
Ola Karlsson <ola.karlsson@bahnhof.se>
2016-09-07 15:17:30 +0200

    Used replace all for whitespace

--- uppgift1/Namn.java
+++ uppgift1/Namn.java
@@ -8,17 +8,7 @@ public class Namn {
 		namn = JOptionPane.showInputDialog("Ange ditt för och efternamn!:");
 		/*toLowercase ger bara gemener, trim tar bort onödig whitespace och replaceAll tar bort
 		 å, ä och ö och ersätter med a och o.*/
-		namn=namn.toLowerCase().trim().replaceAll("å","a").replaceAll("ä","a").replaceAll("ö","o");
-		//Index för förnamnet
-		int findex = namn.indexOf(' ');
-		//Index föe efternamnet
-		int lindex =namn.indexOf(' ')+1;
-		//Plocka ut sista indexet
-		int lastindex =namn.length();
-		//Plocka ut förnamnet
-		fornamn=namn.substring(0,findex);
-		//Plocka ut efternamn
-		efternamn=namn.substring(lindex, lastindex);
-		JOptionPane.showMessageDialog(null,"Din mailadress:\n"+fornamn+"."+efternamn+"@nackademin.se");
+		namn=namn.toLowerCase().trim().replaceAll("å","a").replaceAll("ä","a").replaceAll("ö","o").replaceAll(" ",".");
+		JOptionPane.showMessageDialog(null,"Din mailadress:\n"namn+"@nackademin.se");
 	}
 }
