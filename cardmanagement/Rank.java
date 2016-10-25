a9458cc7395e9534431ca53b4bbb11fbf2ea228e
Ola Karlsson <ola.karlsson@bahnhof.se>
2016-10-25 23:10:18 +0200

    First draft

---
+++ cardmanagement/Rank.java
@@ -0,0 +1,12 @@
+package com.gmail.murmeldjur;
+/**
+ * Enum class for Rank of cards
+ * @author Ola Karlsson
+ * @version 1.0
+ * @since   2016-10-24 
+ *
+ */
+public enum Rank {
+	ESS,TVÅ,TRE,FYRA,FEM,SEX,SJU,ÅTTA,
+	NIO,TIO,KNEKT,DAM,KUNG;
+}
