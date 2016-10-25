8590921d99a33510395d9361cdaff95480961287
Ola Karlsson <ola.karlsson@bahnhof.se>
2016-10-25 23:11:02 +0200

    First draft

---
+++ cardmanagement/Suite.java
@@ -0,0 +1,11 @@
+package com.gmail.murmeldjur;
+/**
+ * Enum class for Suite of cards
+ * @author Ola Karlsson
+ * @version 1.0
+ * @since   2016-10-24 
+ *
+ */
+public enum Suite {
+	RUTER,HJÄRTER,KLÖVER,SPADER;
+}
