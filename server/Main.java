f8f1c3ed21aec44565d55f798518f4aa81b55a10
Ola Karlsson <ola.karlsson@bahnhof.se>
2016-12-09 23:08:31 +0100

    First draft

---
+++ server/Main.java
@@ -0,0 +1,23 @@
+package com.gmail.murmeldjur.server;
+
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try (ServerSocket serverSocket = new ServerSocket(61616)) {
+            System.out.println("Server listening...");
+
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                System.out.println("Server connected.");
+                new Thread(new ContactServer(clientSocket)).start();
+            }
+        } catch (IOException e) {
+            System.err.println("Contact Server got an IOException" + e.getStackTrace());
+        }
+    }
+}
